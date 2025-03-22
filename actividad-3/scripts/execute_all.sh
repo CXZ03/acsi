01a0cb42cd2ad9a5b0b9122720ddaacb42124b56
Andres95123 <andrescpcoplldenrabassa@gmail.com>
2025-03-22 13:44:31 +0100

    Loads modify

--- actividad-3/scripts/execute_all.sh
+++ actividad-3/scripts/execute_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-threads="1 2 4 6 12"
-loads="25000 50000 75000"
+threads="1 2 3 4 6 9 12"
+loads="12500 25000 37500 50000 62500 75000"
 time_interval=1
 
 
-bash overhead-monitor.sh overhead-all.txt sys-executer.sh "$loads" "$threads" $time_interval
+LC_NUMERIC=C bash overhead-monitor.sh overhead-all.txt sys-executer.sh "$loads" "$threads" $time_interval
